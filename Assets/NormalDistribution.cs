513d973c2c4f06756ab9c22e132420f4f949ec8e
adarshkhare1 <adarshkhare@hotmail.com>
2022-01-12 00:26:03 -0800

    Minor button graphics update.

---
+++ Assets/NormalDistribution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class NormalDistribution
+{
+    private float _variance = 0.05f;
+    private float _mean;
+
+   
+    public NormalDistribution(float minValue = 0.0f, float maxValue = 1.0f)
+    {
+        _mean = (minValue+ maxValue) / 2;
+        _variance = (maxValue - Mean ) / 3.0f;
+    }
+
+    public NormalDistribution(float mean)
+    {
+        _mean = mean;
+    }
+
+    internal float Mean { get => _mean; }
+
+    internal float RandomGaussian()
+    {
+        float x1, x2, w, y1; //, y2;
+
+        do
+        {
+            x1 = 2f * (float)UnityEngine.Random.value - 1f;
+            x2 = 2f * (float)UnityEngine.Random.value - 1f;
+            w = x1 * x1 + x2 * x2;
+        } while (w >= 1f);
+
+        w = Mathf.Sqrt((-2f * Mathf.Log(w)) / w);
+        y1 = x1 * w;
+        return (y1 * _variance) + _mean;
+    }
+}
+
